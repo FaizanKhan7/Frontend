5ee437719a81327e25ba990731350fbdeaeb436f
Vipul Sharma <vipulsnp@gmail.com>
2020-09-23 01:06:25 +0530

    Added repo Fetching in project info page

--- pages/project/[pid].jsx
+++ pages/project/[pid].jsx
@@ -1,51 +1,33 @@
 import Router from 'next/router';
 import React, { useEffect, useState } from 'react';
-// import { toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 
 import Discussion from '../../src/components/Feed/Discussion';
 import Issues from '../../src/components/Feed/Issues';
 import PullRequests from '../../src/components/Feed/Pull-requests';
 import Header from '../../src/components/Header';
-// import Spinner from '../../src/components/Spinner';
+import Spinner from '../../src/components/Spinner';
 import styles from '../../src/scss/project.module.scss';
-// import * as feedService from '../../src/services/feed';
+import * as feedService from '../../src/services/feed';
 
 const project = () => {
   const [repoUrl, setRepoUrl] = useState(null);
   const [pageLoading, setPageLoading] = useState(true);
-  // const [pageNo, setPageNo] = useState(1); // Node id to start after
-  // const [repoList, setRepoList] = useState([]); // All Repositories List
-  // const [reachedEnd, setReachedEnd] = useState(false); // Infinite Scrolling : End Reached
-  // const [reposLoading, setReposLoading] = useState(false);
+  const [repoData, setRepoData] = useState(null);
 
-  // async function getRepoInfo() {
-  //   try {
-  //     const res = feedService.getRepo(owner, name);
-  //     if (res.status === 200)
-  //       res.data &&
-  //         res.data.data &&
-  //         res.data.data.items &&
-  //         setRepoList([...repoList, res.data.data.items].flat());
-  //     if (res.data.hasNextPage === false) {
-  //       setReachedEnd(true);
-  //     }
-  //     setPageNo(pageNo + 1);
-  //   } catch (res) {
-  //     toast.error(`${res.status} : ${res.message}`);
-  //     setReachedEnd(true);
-  //   }
-  //   setPageLoading(false);
-  //   setReposLoading(false);
-  // }
-  //   useEffect(() => {
-  //   if (paramsChanged !== null) getNextRepos();
-  //   if (firstResult.current) {
-  //     window.scrollTo({
-  //       top: firstResult.current.offsetTop,
-  //       behavior: 'smooth'
-  //     });
-  //   }
-  // }, [paramsChanged]);
+  async function getRepoInfo() {
+    try {
+      const res = await feedService.getRepo(
+        Router.query.pid.split(' ')[0],
+        Router.query.pid.split(' ')[1]
+      );
+      if (res.status === 200)
+        res.data && res.data.data && setRepoData(res.data.data);
+    } catch (res) {
+      toast.error(`${res.status} : ${res.message}`);
+    }
+    setPageLoading(false);
+  }
 
   useEffect(() => {
     if (Router.query.pid) {
@@ -54,7 +36,7 @@ const project = () => {
           Router.query.pid.split(' ')[1]
         }`
       );
-      setPageLoading(false);
+      getRepoInfo();
     }
   }, []);
 
@@ -64,63 +46,72 @@ const project = () => {
     setTab(tab);
   };
 
-  // if (pageLoading) {
-  //   return <Spinner />;
-  // }
+  if (pageLoading) return <Spinner />;
 
   return (
     <div>
       <Header />
       <div className={styles.projectProfile}>
         <div className={styles['card-left-column']}>
-          {/* <h1></h1> */}
+          <h1>{repoData.full_name && repoData.full_name.split('/')[1]}</h1>
           <div className={styles['card-left-info']}>
             <div className={styles['org-languages']}>
               <p>
                 By{' '}
                 <em style={{ color: 'green' }}>
-                  {/* {Router.query.pid.split(' ')[0]} */}
+                  {repoData.full_name && repoData.full_name.split('/')[0]}
                 </em>{' '}
                 | Updated:{' '}
-                {/* {repoNames.updated_at && repoNames.updated_at.slice(0, 10)} */}
+                {repoData.updated_at && repoData.updated_at.slice(0, 10)}
               </p>
               <span className={styles['org-lang-1']}>
-                {/* {Router.query.pid.language} */}
-              </span>
-              <span className={styles['org-lang-2']}>
-                {/* {Router.query.pid.language} */}
+                {repoData.language && repoData.language}
               </span>
             </div>
             <p>{Router.query.pid.description}</p>
             <div className={styles['git-dev-icons']}>
-              {/* <a href={Router.query.pid.html_url} target="_blank"> */}
-              <button className={styles['github-icon']}>
-                <img src="/icons/github-icon.png" alt="Github-icon" />
-                <p>Github</p>
-              </button>
-              {/* </a> */}
-              {/* <a href={Router.query.pid.html_url} target="_blank"> */}
-              <button className={styles['gitpod-icon']}>
-                <img src="/icons/gitPod-icon.png" alt="Gitpod-icon" />
-                <p>Gitpod</p>
-              </button>
-              {/* </a> */}
+              <a
+                href={repoData.html_url && repoData.html_url}
+                target="_blank"
+                rel="noreferrer">
+                <button
+                  type="button"
+                  className={styles['github-icon']}
+                  disabled={!repoData.html_url}>
+                  <img src="/icons/github-icon.png" alt="Github-icon" />
+                  <p>Github</p>
+                </button>
+              </a>
+              <a
+                href={repoData.gitPodUrl && repoData.gitPodUrl}
+                target="_blank"
+                rel="noreferrer">
+                <button
+                  type="button"
+                  className={styles['gitpod-icon']}
+                  disabled={!repoData.gitPodUrl}>
+                  <img src="/icons/gitPod-icon.png" alt="Gitpod-icon" />
+                  <p>Gitpod</p>
+                </button>
+              </a>
             </div>
           </div>
         </div>
         <div className={styles['card-right-column']}>
           <div className={styles['forks-and-star']}>
             <img src="/icons/star-icon.png" alt="Star" />
-            <p>Star</p>
+            <p>Stars</p>
             <div className={styles['total-forks-and-stars']}>
-              <p>148k</p>
+              <p>
+                {repoData.stargazers_count ? repoData.stargazers_count : '0'}
+              </p>
             </div>
           </div>
           <div className={styles['forks-and-star']}>
             <img src="/icons/fork-icon.png" alt="Fork" />
-            <p>Fork</p>
+            <p>Forks</p>
             <div className={styles['total-forks-and-stars']}>
-              <p>148k</p>
+              <p>{repoData.forks ? repoData.forks : '0'}</p>
             </div>
           </div>
         </div>
